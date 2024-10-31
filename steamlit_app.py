7613e4548763992fe8f1ae188ae098f6bede54fb
JulienRapacki <julien.rapacki@gmail.com>
2024-10-31 08:27:12 +0100

    display modif

--- steamlit_app.py
+++ steamlit_app.py
@@ -28,21 +28,35 @@ def load_file():
     return data
 
 
-data = load_file()
-data_sample = data.sample(50)
 
+if 'data' not in st.session_state:
+    st.session_state.data = load_file()
+    st.session_state.data_sample = st.session_state.data.sample(50)
+
+data = st.session_state.data
+data_sample = st.session_state.data_sample
 
 st.markdown("<h1 style='color: #7350EA;'>Tableau de bord Projet 9 :\n Analyse de sentiments avec le Deep learning</h1>", unsafe_allow_html=True)
 
 st.write('### Aperçu des données')       
 st.dataframe(data_sample,use_container_width= True)
 
-col1,col2 = st.columns([.5,.5],gap='medium',
+col1,col2,col3 = st.columns([.5,.25,.25],gap='medium',
                         vertical_alignment= "bottom")
 
-
+#WordCloud
 with col1:
-    col1.subheader('Distribution de la Longueur des Phrases')
+    col1.subheader('Nuage de Mots')
+    text_cloud = " ".join(data_sample['text'])
+    wordcloud = WordCloud(width=800, height=400, background_color='white',colormap = 'Set2').generate(text_cloud)
+    fig, ax = plt.subplots(figsize=(8, 4))
+    ax.imshow(wordcloud, interpolation='bilinear')
+    ax.axis("off")
+    st.pyplot(fig)
+
+# Histogram
+with col2:
+    col2.subheader('Distribution de la Longueur des Phrases')
     data['txt_length'] = data['text'].apply(len)
     fig1 = px.histogram(data, x='txt_length', nbins=10, 
     labels={'txt_length': 'Longueur des phrases'},
@@ -50,25 +64,22 @@ with col1:
     st.write(fig1)
 
 # Histogram
-with col2:
-    col2.subheader('Distribution des classes ')
+with col3:
+    col3.subheader('Distribution des classes ')
     fig = px.histogram(
             data_frame=data, x="sentiment",
             color_discrete_sequence=['#7350EA'])
     st.write(fig)
 
-#WordCloud
-st.write('### Nuage de Mots')
-text_cloud = " ".join(data_sample['text'])
-wordcloud = WordCloud(width=800, height=400, background_color='white',colormap = 'Set2').generate(text_cloud)
-fig, ax = plt.subplots(figsize=(8, 4))
-ax.imshow(wordcloud, interpolation='bilinear')
-ax.axis("off")
-st.pyplot(fig)
+
+
 
 # URL API Azure
 API_URL = "https://apip09.azurewebsites.net/predict"
+# API_URL = "http://127.0.0.1:5000/predict"
 API_INTERPRET = "https://apip09.azurewebsites.net/interpret"
+# API_INTERPRET = "http://127.0.0.1:5000/predict/interpret"
+
 # Fonction pour analyser le sentiment
 st.write('### Test du modèle')
 
@@ -82,21 +93,19 @@ def analyze_sentiment():
             
     st.session_state.sentiment= result['sentiment']        
     st.write(f"**Résultat de l'analyse :** {st.session_state.sentiment}")       
-
-# Afficher le sentiment si déjà calculé
-# if 'sentiment' in st.session_state:
-#     st.write(f"**Résultat de l'analyse :** {st.session_state.sentiment}")
     
 
 # Bouton pour analyser
 if st.button("Analyser"):
     analyze_sentiment()
 
+# Bouton pour interpréter
 if st.button("Interpretation"):
-    response = requests.post(f"{API_INTERPRET}", json={"text": user_input})
-    result = response.json()
-    df  = pd.DataFrame(result.get('interpretation'))
-    st.session_state.interpretation = result['interpretation']
+    if 'interpretation' not in st.session_state or st.session_state.interpretation != user_input:
+        response = requests.post(f"{API_INTERPRET}", json={"text": user_input})
+        result = response.json()
+        df  = pd.DataFrame(result.get('interpretation'))
+        st.session_state.interpretation = result['interpretation']
        
     # Affiche l'interpretation
 
@@ -111,9 +120,13 @@ if st.button("Interpretation"):
         """,
         unsafe_allow_html=True
     )
+    if 'interpretation' in st.session_state:
+        df = pd.DataFrame(st.session_state.interpretation)
+        fig2 = px.bar(data_frame=df, x="contribution", y="word", color_discrete_sequence=['#7350EA'])
+        st.write(fig2)
     
-    fig2 = px.bar(
-    data_frame= df, x="contribution", y="word",
-    color_discrete_sequence=['#7350EA'])
-    st.write(fig2)
+    # fig2 = px.bar(
+    # data_frame= df, x="contribution", y="word",
+    # color_discrete_sequence=['#7350EA'])
+    # st.write(fig2)
 
