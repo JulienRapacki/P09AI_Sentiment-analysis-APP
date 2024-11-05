82103a7959f74c9c7a9fe94b8a441d93181d47e8
JulienRapacki <julien.rapacki@gmail.com>
2024-11-05 09:02:58 +0100

    display modif

--- steamlit_app.py
+++ steamlit_app.py
@@ -11,7 +11,6 @@ from azure.storage.blob import BlobServiceClient
 
 
 
-
 st.set_page_config(
     page_title="Tableau de bord Projet 9",
     page_icon="🧊",
@@ -38,7 +37,7 @@ st.write('### Aperçu des données')
 st.dataframe(data_sample,use_container_width= True)
 
 
-
+# définir la disposition des graphiques
 col1,col2,col3 = st.columns([.5,.25,.25],gap='medium',
                         vertical_alignment= "bottom")
 
@@ -57,22 +56,39 @@ with col2:
     col2.subheader('Distribution de la Longueur des Phrases')
     data['txt_length'] = data['text'].apply(len)
     fig1 = px.histogram(data, x='txt_length', nbins=10, 
-    labels={'txt_length': 'Longueur des phrases'},
+    labels={'txt_length': 'Nombre de caractères'},
     color_discrete_sequence=['#7350EA'])
+    fig1.update_xaxes(
+        tickfont = dict(size=15 ,color = 'black'),
+        title_font = dict(size=15 ,color = 'black')
+        )
+    fig1.update_yaxes(
+        tickfont = dict(size=15 ,color = 'black'),
+        title_font = dict(size=15 ,color = 'black')
+        )    
     st.write(fig1)
 
 # Histogram
 with col3:
-    col3.subheader('Distribution des classes ')
-    fig = px.histogram(
+    col3.subheader('Distribution des classes')
+    fig2 = px.histogram(
             data_frame=data, x="sentiment",
             color_discrete_sequence=['#7350EA'])
-    st.write(fig)
+    fig2.update_xaxes(
+        tickfont = dict(size=15 ,color = 'black'),
+        title_font = dict(size=15 ,color = 'black')
+        )
+    fig2.update_yaxes(
+        tickfont = dict(size=15 ,color = 'black'),
+        title_font = dict(size=15 ,color = 'black')
+    )
+    st.write(fig2)
 
 
 # URL API Azure
 API_URL = "https://apip09.azurewebsites.net/predict"
 API_INTERPRET = "https://apip09.azurewebsites.net/interpret"
+
 # Fonction pour analyser le sentiment
 st.write('### Test du modèle')
 
